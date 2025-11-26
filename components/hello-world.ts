e1a725485ab13f0696cf78fd2ff518034bb306cd
Mark Volkmann <r.mark.volkmann@gmail.com>
2025-11-26 08:33:08 -0600

    deleted commented lines in hello-world.ts

--- components/hello-world.ts
+++ components/hello-world.ts
@@ -1,14 +1,11 @@
 import { LitElement, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
-//import { render } from "@lit-labs/ssr";
 
-//TODO: See https://discord.com/channels/1012791295170859069/1047015641225371718/threads/1440831001927483402
 @customElement("hello-world")
 export class HelloWorld extends LitElement {
   @property({ type: String, reflect: true }) name = "World";
 
   render() {
-    //return render(html`
     return html`
       <style>
         :host {
